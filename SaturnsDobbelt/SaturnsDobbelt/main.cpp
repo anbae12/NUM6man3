18bd200f848ee6919af3814779380f5b3edb621c
Anders Launer Bæk <anbae12@student.sdu.dk>
2015-04-15 08:42:05 +0200

    Så vi der klar til den sidste.. alle ikke nødvendige filer bør være tilføjet til .gitignore :)

---
+++ SaturnsDobbelt/SaturnsDobbelt/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  SaturnsDobbelt
+//
+//  Created by Anders Launer Bæk on 15/04/15.
+//  Copyright (c) 2015 Anders Launer Bæk. All rights reserved.
+//
+
+#include <iostream>
+#include "nr3.h"
+
+int main(int argc, const char * argv[]) {
+
+
+    return 0;
+}
