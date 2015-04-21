093d88ba16d489740da4041228b63d24468d9621
Anders Launer Bæk <anbae12@student.sdu.dk>
2015-04-21 11:53:47 +0200

    Sådan mand!

---
+++ plotdif.m
@@ -0,0 +1,35 @@
+clear;
+clc;
+close all
+M=csvread('data.csv');
+%{
+figure(1)
+plot(M(:,1),'r')
+hold on
+plot(M(:,2),'b')
+ylabel('Km')
+xlabel('Days')
+legend('r1','r2','Location','northeast')
+print(gcf,'-depsc','-tiff','Rapport/graphics/cross.eps')
+figure(2)
+plot(M(:,3),'m')
+ylabel('Radians')
+xlabel('Days')
+legend('delta angle','Location','northeast')
+print(gcf,'-depsc','-tiff','Rapport/graphics/phase.eps')
+%}
+figure(3)
+subplot(2,1,1)
+plot(M(:,1),'r')
+hold on
+plot(M(:,2),'b')
+ylabel('Km')
+xlabel('Days')
+legend('r1','r2','Location','northeast')
+print(gcf,'-depsc','-tiff','cross.eps')
+subplot(2,1,2)
+plot(M(:,3),'m')
+ylabel('Radians')
+xlabel('Days')
+legend('delta angle','Location','northeast')
+print(gcf,'-depsc','-tiff','Rapport/graphics/subplot.eps')
